aa2dbfff14046660ed60344b964a18e0d8a1a414
dwandika <mochamaddwiandika2@gmail.com>
2024-09-24 15:25:43 +0700

    Andika

---
+++ src/belajarlagi/Andika.java
@@ -0,0 +1,18 @@
+package belajarlagi;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Andika
+ * 
+ */
+public class Andika {
+    public static void main(String[] args) {
+        System.out.println("Andika");
+    }
+  
+}
